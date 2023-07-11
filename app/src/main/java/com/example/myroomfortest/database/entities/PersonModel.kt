3f8867d162a32c981ec3bb189ee4ee0ba7a176e6
Reham Yahia <asmaa115yahia@gmail.com>
2023-07-11 23:41:34 +0200

    update feature

--- app/src/main/java/com/example/myroomfortest/database/entities/PersonModel.kt
+++ app/src/main/java/com/example/myroomfortest/database/entities/PersonModel.kt
@@ -4,22 +4,24 @@ import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parceler
 import kotlinx.parcelize.Parcelize
 
 
-@Parcelize
+//@Parcelize
 @Entity(tableName = "personTable")
 data class PersonModel (@PrimaryKey(autoGenerate = true)val id:Int, val firstName:String, val lastName:String, val age:String)
-    : Parcelable {
+//    : Parcelable {
 
 
-    private companion object : Parceler<PersonModel> {
-        fun PersonModel.write(parcel: Parcel, flags: Int) {
-            // Custom write implementation
-        }
-
-        fun create(parcel: Parcel): PersonModel {
-            // Custom read implementation
-        }
-    }
-}
+//    private companion object : Parceler<PersonModel> {
+//        override fun PersonModel.write(parcel: Parcel, flags: Int) {
+//            // Custom write implementation
+//        }
+//
+//        override fun create(parcel: Parcel): PersonModel {
+//            // Custom read implementation
+//            return PersonModel()
+//        }
+//    }
+//}
