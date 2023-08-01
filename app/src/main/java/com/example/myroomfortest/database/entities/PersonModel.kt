7731f72267bbeb3eeb216c622fe4255aaf51558d
Reham Yahia <asmaa115yahia@gmail.com>
2023-08-01 15:35:51 +0200

    update

--- app/src/main/java/com/example/myroomfortest/database/entities/PersonModel.kt
+++ app/src/main/java/com/example/myroomfortest/database/entities/PersonModel.kt
@@ -4,7 +4,6 @@ import android.graphics.Bitmap
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import android.os.Parcelable
-import androidx.room.Embedded
 import kotlinx.parcelize.Parcelize
 
 
